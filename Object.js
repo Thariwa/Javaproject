5f7a12a7a9a3ac4d6e901a381dec2b59f6264056
Thariwa <umarthariwa@gmail.com>
2023-05-11 12:52:39 -0700

    feat:Index files

---
+++ Object.js
@@ -0,0 +1,92 @@
+const cohort6B2 = [
+    {
+        firstname:'Thariwa',
+        surname: 'Umar',
+        DOB: 'Apr-16',
+        state: 'Jigawa',
+        gender:'Female',
+        Hobbies: ['Reading', 'Swimming', 'Laughing']
+
+    },
+    {
+       firstname: 'Samiat',
+       surname: 'Usman',
+       DOB: 'Apr-21',
+       state:'Kwara',
+       gender: 'Female',
+       Hobbies: ['sleeping', 'exploring', 'eating']
+    },
+    {
+       firstname: 'Chidinma',
+       surname: 'Nwaneti',
+       DOB: 'Dec-12',
+       state:'Anambra',
+       gender: 'Female',
+       Hobbies: ['swimming', 'playing games', 'shopping']
+    },
+    {
+       firstname: 'Sharon',
+       surname: 'ibanga',
+       DOB: 'Dec-28',
+       state:'Akwa-ibom',
+       gender: 'Female',
+       Hobbies: ['sleeping', 'coding', 'eating']
+    },
+    {
+       firstname: 'Damola',
+       surname: 'Gbenga',
+       DOB: 'Nov-18',
+       state:'Ekiti',
+       gender: 'Male',
+       Hobbies: ['Worshiping', 'exploring', 'Coding']
+    },
+    {
+       firstname: 'Steven',
+       surname: 'Momoh',
+       DOB: '07-12',
+       state:'Edo',
+       gender: 'Male',
+       Hobbies: ['Gaming', 'Art', 'Coding']
+    },
+    {
+       firstname: 'Benson',
+       surname: 'Ibeabuchi',
+       DOB: '07-03',
+       state:'Abia',
+       gender: 'Male',
+       Hobbies: ['photography', 'programming', 'traveling']
+    },
+    {
+       firstname: 'Amira',
+       surname: 'Olusesi',
+       DOB: 'Apr-24',
+       state:'Lagos',
+       gender: 'Female',
+       Hobbies: ['Wearing black', 'Living life', 'eating']
+    },
+    {
+       firstname: 'Kelubia',
+       surname: 'Urubusi',
+       DOB: 'Mar-08',
+       state:'Delta',
+       gender: 'Male',
+       Hobbies: ['Fornicating', 'Drinking', 'Smoking']
+    },
+    {
+       firstname: 'Gideon',
+       surname: 'Owa',
+       DOB: 'Jul-17',
+       state:'Ondo',
+       gender: 'Male',
+       Hobbies: ['Coding', 'Gaming', 'Living']
+    },
+    {
+       firstname: 'Marianna',
+       surname: 'Pat',
+       DOB: 'Aug-31',
+       state:'Kwara',
+       gender: 'Female',
+       Hobbies: ['sleeping', 'exploring', 'eating']
+    },
+
+]
